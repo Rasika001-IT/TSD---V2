9ab5e4595aa910d5fbf3587e77ad46a4772be382
RASIKA-SPACE-IT <rasika.it001@gmail.com>
2026-04-20 22:14:03 +0530

    Delete frontend directory
    deleting for deployment

--- frontend/src/components/layout/SectionWrapper.jsx
+++
@@ -1,9 +0,0 @@
-const SectionWrapper = ({ children, className = "" }) => {
-  return (
-    <section className={`py-16 ${className}`}>
-      {children}
-    </section>
-  );
-};
-
-export default SectionWrapper;
